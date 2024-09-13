c9021bd9c0ceae69e59d951b768cf5459b0efdf8
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-09-13 09:03:07 +0530

    Add files via upload

---
+++ do-whileloop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the no: ";
+    cin>>n;
+    int i=16;
+    do{
+        cout<<i<<endl;
+        i++;
+    }
+    while(i<=n);
+}

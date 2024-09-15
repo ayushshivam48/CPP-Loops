09dc3eda6ad13cfa9f0ec8a4f8f9dd533d9021d7
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-09-15 11:42:27 +0530

    Add files via upload

---
+++ sumofevendigits.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the no: ";
+    cin>>n;
+    int sum=0;
+    while(n!=0){
+        int ld=n%10;
+       n=n/10;
+       if(ld%2==0){
+        sum += ld;
+        }
+      
+    }
+    cout<<sum;
+}
